fcd2483ba65481e0fb0b4bd7aa99a31f8cb75323
Roney V. Silva <roneyvsilva@gmail.com>
2023-04-04 10:21:57 -0300

    Feat: habilitando autenticação na rota

--- src/router/categoria.router.js
+++ src/router/categoria.router.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
 const categoriaController = require("../controller/categoria.controller");
-//const authMiddleware = require("../middleware/auth.middleware");
+const authMiddleware = require("../middleware/auth.middleware");
 const { validaCategoria, validaIdParams } = require("../middleware/validacao.middleware");
 const paginacao = require("../middleware/paginacao.middleware");
 
 // rotas GET
-router.get("/findById/:id", /* authMiddleware, */ validaIdParams, categoriaController.findCategoriaByIdController);
-router.get("/findAll", /* authMiddleware, */paginacao, categoriaController.findAllCategoriaController);
+router.get("/findById/:id", authMiddleware, validaIdParams, categoriaController.findCategoriaByIdController);
+router.get("/findAll", authMiddleware, paginacao, categoriaController.findAllCategoriaController);
 // rotas POST
-router.post("/create", /* authMiddleware, */validaCategoria, categoriaController.createCategoriaController);
+router.post("/create", authMiddleware, validaCategoria, categoriaController.createCategoriaController);
 // rotas PUT
-router.put("/update/:id", /* authMiddleware,*/ validaIdParams, validaCategoria, categoriaController.updateCategoriaController);
+router.put("/update/:id", authMiddleware, validaIdParams, validaCategoria, categoriaController.updateCategoriaController);
 //rotas DELETE
-router.delete("/remove/:id", /* authMiddleware, */validaIdParams, categoriaController.deleteCategoriaController);
+router.delete("/remove/:id", authMiddleware, validaIdParams, categoriaController.deleteCategoriaController);
 
 module.exports = router;
