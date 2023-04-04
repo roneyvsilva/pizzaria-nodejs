9cee3c2274521c4ab0c40109886de98b4f162378
Roney V. Silva <roneyvsilva@gmail.com>
2023-04-04 09:45:42 -0300

    Fix: correção msg de retorno de login inválido

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -7,13 +7,13 @@ const loginController = async (req, res) => {
     const usuario = await authService.loginService(email);
 
     if (!usuario) {
-        return res.status(400).send({ message: "Usuário não encontrado." });
+        return res.status(400).send({ message: "Usuário e/ou senha inválido(s)." });
     }
 
     const senhaValida = await bcrypt.compare(senha, usuario.senha);
 
     if (!senhaValida) {
-        return res.status(400).send({ message: "Senha inválida." });
+        return res.status(400).send({ message: "Usuário e/ou senha inválido(s)." });
     }
 
     const token = authService.generateToken(usuario.id);
