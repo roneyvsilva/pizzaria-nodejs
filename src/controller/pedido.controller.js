fb1f9d52547248a9bab6354114d12b44674b9e13
Roney V. Silva <roneyvsilva@gmail.com>
2023-04-05 14:54:41 -0300

    Fix: validação de retorno do find Pedido

--- src/controller/pedido.controller.js
+++ src/controller/pedido.controller.js
@@ -2,7 +2,12 @@ const pedidoService = require("../service/pedido.service");
 
 const findPedidoByIdController = async (req, res) => {
     try {
-        return res.status(200).send(await pedidoService.findPedidoByIdService(req.params.id));
+        const retorno = await pedidoService.findPedidoByIdService(req.params.id);
+        if (retorno != null) {
+            return res.status(200).send(retorno);
+        } else {
+            return res.status(404).send({ message: `Pedido não encontrado.` });
+        }
     } catch (e) {
         if (e.kind == "ObjectId") {
             return res.status(400).send({ message: `ID informado está incorreto.` });
@@ -14,7 +19,12 @@ const findPedidoByIdController = async (req, res) => {
 
 const findAllPedidoController = async (req, res) => {
     try {
-        return res.status(200).send(await pedidoService.findAllPedidoService(req.query.limit, req.query.offset));
+        const retorno = await pedidoService.findAllPedidoService(req.query.limit, req.query.offset);
+        if (retorno.length > 0) {
+            return res.status(200).send(retorno);
+        } else {
+            return res.status(404).send({ message: `Nenhum Pedido encontrado.` });
+        }
     } catch (e) {
         console.log(`Erro: ${e.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
@@ -37,7 +47,12 @@ const createPedidoController = async (req, res) => {
 
 const deletePedidoController = async (req, res) => {
     try {
-        return res.status(200).send(await pedidoService.deletePedidoService(req.params.id));
+        const retorno = await pedidoService.deletePedidoService(req.params.id);
+        if (retorno != null) {
+            return res.status(200).send(retorno);
+        } else {
+            return res.status(404).send({ message: `Pedido não encontrado.` });
+        }
     } catch (e) {
         console.log(`Erro: ${e.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
@@ -46,7 +61,12 @@ const deletePedidoController = async (req, res) => {
 
 const udpateStatusPedidoController = async (req, res) => {
     try {
-        return res.status(200).send(await pedidoService.updateStatusPedidoService(req.params.id));
+        const retorno = await pedidoService.updateStatusPedidoService(req.params.id);
+        if (retorno != null) {
+            return res.status(200).send(retorno);
+        } else {
+            return res.status(404).send({ message: `Pedido não encontrado.` });
+        }
     } catch (e) {
         console.log(`Erro: ${e.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
